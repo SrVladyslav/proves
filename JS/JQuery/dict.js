76c832723684bca75082aa23ac8a3ebbce79652b
SrVladyslav <vlama@inf.upv.es>
2020-03-24 15:47:18 +0100

    adding dictionary

---
+++ JS/JQuery/dict.js
@@ -0,0 +1,12 @@
+var dictionary = {
+	'title':{
+		'es':'Titulo',
+		'en':'Title',
+		'ca':'Titol'
+	},
+	'text':{
+		'en':'Text: it is just a POC',
+		'es':'Texto: solo es una prueba de concepto',
+		'ca':'Text: a soles es una prova de concepte'
+	}
+}
